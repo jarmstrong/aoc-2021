913c7303b1d4b258b56815a619307275a1582ee2
James Armstrong <itsjarm@gmail.com>
2021-12-07 17:22:22 +1100

    Day7 both solutions

---
+++ src/Day07.kt
@@ -0,0 +1,45 @@
+import kotlin.math.absoluteValue
+
+private fun readCrabPositions(filename: String): List<Int> {
+    return readInput(filename).first().split(',').map { it.toInt() }
+}
+
+fun calculateMinimumFuelCost(input: List<Int>, fuelCostToTarget: (Int, Int) -> Int): Int {
+    val maxCrabPosition = input.maxOf { it }
+
+    val fuelCostsPerTargetPosition = mutableMapOf<Int, Int>()
+
+    (0..maxCrabPosition).forEach { targetCrabPosition ->
+        input.forEach { currentCrabPosition ->
+            val fuelCostToTargetCrabPosition = fuelCostToTarget(currentCrabPosition, targetCrabPosition)
+
+            fuelCostsPerTargetPosition[targetCrabPosition] =
+                fuelCostsPerTargetPosition.getOrDefault(targetCrabPosition, 0) + fuelCostToTargetCrabPosition
+        }
+    }
+
+    return fuelCostsPerTargetPosition.minOf { it.value }
+}
+
+fun main() {
+    fun part1(input: List<Int>): Int {
+        return calculateMinimumFuelCost(input) { currentCrabPosition, targetCrabPosition ->
+            (currentCrabPosition - targetCrabPosition).absoluteValue
+        }
+    }
+
+    fun part2(input: List<Int>): Int {
+        return calculateMinimumFuelCost(input) { currentCrabPosition, targetCrabPosition ->
+            val distanceToTargetCragPosition = (currentCrabPosition - targetCrabPosition).absoluteValue
+            distanceToTargetCragPosition * (distanceToTargetCragPosition + 1) / 2
+        }
+    }
+
+    val testInput = readCrabPositions("Day07_test")
+    check(part1(testInput) == 37)
+    check(part2(testInput) == 168)
+
+    val input = readCrabPositions("Day07")
+    println(part1(input))
+    println(part2(input))
+}
